9da1dc4a0f306ac147d39e6818b914a07bedc6b6
ananas2109 <kweanhngo@gmail.com>
2024-01-27 15:52:21 -0500

    NavOptions

--- electrober/App.js
+++ electrober/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { SafeAreaFrameContext } from 'react-native-safe-area-context';
 
 // 1) set up redux
 
---
+++ electrober/components/MapScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, text, View} from "react-native";
+
+const MapScreeen = () => {
+    return (
+        <View>
+            <text>Here is the map stuff...</text>
+        </View>
+    );
+};
+
+export default MapScreeen;
+const styles = StyleSheet.create({});
