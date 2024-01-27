035f687d782620306c11ce5c3016db8601b90372
ananas2109 <kweanhngo@gmail.com>
2024-01-27 14:26:41 -0500

    initialising the Build

--- electrober/App.js
+++ electrober/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// 1) set up redux
+
 export default function App() {
   return (
     <View style={styles.container}>
