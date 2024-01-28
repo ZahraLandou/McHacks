eabd49ff6792bb4449fff59eaec98e9338e442b8
hivakarami <hiva.karami2@gmail.com>
2024-01-27 21:32:18 -0500

    google

--- electrober/components/NavOptions.js
+++ electrober/components/NavOptions.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { FlatList, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import tw from "tailwind-react-native-classnames";
 import {Icon} from "react-native-elements";
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
     {
         id: "123",
         title: "Get a ride",
         image: "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberX.png",
-        screen: "MapScreeen",
+        screen: "MapScreen",
     },
     {
         id: "456",
@@ -19,13 +20,18 @@ const data = [
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
     return (
         <FlatList
         data ={data}
         horizontal
         keyExtractor = {(item) => item.id}
         renderItem={({ item }) => (
-            <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+            <TouchableOpacity
+            onPress={() => navigation.navigate(item.screen)}
+             style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} 
+             >
+                
                 <View>
                     <Image
                     style = {{width: 120, height: 120, resizeMode: "contain"}}
--- electrober/screens/HomeScreen.js
+++ electrober/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPA_APIKEY} from "@env"
 
 const HomeScreen = () => {
     return (
@@ -18,6 +20,14 @@ const HomeScreen = () => {
                 url:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          placeholder="Where are you?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        
+        />
+
         <NavOptions/>
        </View>
      </SafeAreaView>
