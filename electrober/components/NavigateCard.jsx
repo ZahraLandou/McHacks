ad17bcad696cc7b782a020067ce27d1048ea05ef
ananas2109 <kweanhngo@gmail.com>
2024-01-28 03:13:27 -0500

    fixed mapscreen

--- electrober/components/NavigateCard.jsx
+++ electrober/components/NavigateCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import tw from "tailwind-react-native-classnames";
+import {GOOGLE_MAPS_APIKEY} from "@env";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const NavigateCard = () => {
     return(
@@ -8,6 +10,12 @@ const NavigateCard = () => {
             <Text style={tw`text-center py-5 text-xl`}>
                 YO
             </Text>
+            <View style ={tw `border-t border-gray-200 flex-shrink`}>
+                <View>
+                    <GooglePlacesAutocomplete
+                    placeholder = 'Where to?'/>
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
--- electrober/screens/MapScreen.jsx
+++ electrober/screens/MapScreen.jsx
@@ -4,17 +4,17 @@ import { StyleSheet, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from '../components/RideOptionsCard'
 
 const MapScreen = () => {
     const Stack = createStackNavigator;
     return (
         <View>
-
             <View style={tw`h-1/2`}>
                 <Map />
             </View>
 
-            <View style={tw`h-1/2`}>
+            {/* <View style={tw`h-1/2`}>
                 <Stack.Navigator>
                     <Stack.Screen
                         name="NavigateCard"
@@ -25,13 +25,13 @@ const MapScreen = () => {
                     />
                     <Stack.Screen
                         name="RideOptionsCard"
-                        component={NavigateCard}
+                        component={RideOptionsCard}
                         options={{
                             headerShown: false,
                         }}
                     />
                 </Stack.Navigator>
-            </View>
+            </View> */}
         </View>
     );
 };
